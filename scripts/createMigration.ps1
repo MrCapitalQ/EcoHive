eb65ba730d4bc0f2c75c90633059e41a1549d71b
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2023-10-11 12:24:09 -0400

    Move local scripts to scripts directory.

--- scripts/createMigration.ps1
+++ scripts/createMigration.ps1
@@ -8,7 +8,7 @@ $currentDir = Get-Location
 
 try {
     Set-Location $PSScriptRoot
-    Set-Location ..\..
+    Set-Location ..
 
     dotnet ef migrations add $name --startup-project .\src\MrCapitalQ.EcoHive.Api --project .\src\MrCapitalQ.EcoHive.EcoBee.AspNetCore
 }
--- scripts/updateDb.ps1
+++ scripts/updateDb.ps1
@@ -2,7 +2,7 @@ $currentDir = Get-Location
 
 try {
     Set-Location $PSScriptRoot
-    Set-Location ..\..
+    Set-Location ..
 
     dotnet ef database update --startup-project .\src\MrCapitalQ.EcoHive.Api --project .\src\MrCapitalQ.EcoHive.EcoBee.AspNetCore
 }
