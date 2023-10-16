8f48257ddf32d7631dde903457cd8bdcc65c914c
MrCapitalQ <66032879+MrCapitalQ@users.noreply.github.com>
2023-10-16 18:28:57 -0400

    Show Swagger at root.

--- src/MrCapitalQ.EcoHive.Api/Program.cs
+++ src/MrCapitalQ.EcoHive.Api/Program.cs
@@ -22,6 +22,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+app.MapGet("/", c => { c.Response.Redirect("/swagger"); return Task.CompletedTask; });
 
 app.UseAuthorization();
 
